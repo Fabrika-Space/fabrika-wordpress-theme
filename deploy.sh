b5ec1268cbe3e4398322faeaba9239d639d405c9
Vlad Tsepelev <reg.yeti@gmail.com>
2015-11-11 23:47:55 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+ssh ubuntu@$107.21.123.188 << EOF
+
+  # setup ssh keys
+  # NOTE: this section needed to add needed deploy keys
+  ssh-agent bash
+  ssh-add -D
+  ssh-add /home/ubuntu/.ssh/github_rsa
+  
+  echo '1. Updating sources'
+  cd ~/repos/fabrika-wordpress-theme
+  git fetch origin master
+  git checkout --force origin/master
+
+  echo "2. Restart apache"
+  sudo apache2ctl restart
+
+  echo 'Done!'
+  
+EOF
