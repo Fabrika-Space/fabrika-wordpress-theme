26e34aa5da5ab47a263c966f17f306995ff5d60c
Vlad Tsepelev <reg.yeti@gmail.com>
2015-11-12 12:31:42 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,8 +11,8 @@ ssh -v ubuntu@107.21.123.188 << EOF
   
   echo '1. Updating sources'
   cd ~/repos/fabrika-wordpress-theme
-  git fetch origin master
-  git checkout --force origin/master
+  git checkout --force master
+  git pull
 
   echo "2. Restart apache"
   sudo apache2ctl restart
