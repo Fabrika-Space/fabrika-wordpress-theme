ec5cdd2c8089e5fa1d944e8f7f1bc514e2c5ef20
Johndegree <john@toptechphoto.com>
2015-11-13 15:41:45 +0200

    Add phone number

--- fabrika.space/footer.php
+++ fabrika.space/footer.php
@@ -11,7 +11,7 @@
 					<div class="item"><span class="icon_location">Харьков,<br /> ул. Карла Маркса, 1</span></div>
 				</div>
 				<div class="part">
-					<!--div class="item"><a href="tel:80501234567" class="icon_tel">8 (050) 123 45 67</a></div-->
+					<div class="item"><a href="tel:+380681001144" class="icon_tel">8 (050) 123 45 67</a></div>
 					<div class="item"><a href="mailto:info@fabrika.space" class="icon_mail">info@fabrika.space</a></div>
 				</div>
 				<!--div class="part">
