b024d0d6ebc83146a96efb620b90641617c83736
NightNei <nei.css@gmail.com>
2015-11-13 14:04:54 +0200

    change title

--- fabrika.space/header.php
+++ fabrika.space/header.php
@@ -9,7 +9,7 @@
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
-	<title>FABRIKA.SPACE | коворкинг | ивент-зона | бар</title>
+	<title>Fabrika.space | коворкинг | ивент-зона | бар</title>
 	<meta name="description" content="FABRIKA.SPACE – хаб притяжения умов, мастерская идей и качественного отдыха">
 	<link rel="image_src" href="/wp-content/themes/fabrika.space/img/cover.jpg">
 	<meta itemprop="image" content="/wp-content/themes/fabrika.space/img/cover.jpg">
