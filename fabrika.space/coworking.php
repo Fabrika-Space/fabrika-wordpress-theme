a2a866a87c45dc8c5ae9435c82f4312d26af0348
NightNei <nei.css@gmail.com>
2015-11-13 18:23:00 +0200

    add coworking page

---
+++ fabrika.space/coworking.php
@@ -0,0 +1,199 @@
+<?php get_header(); ?>
+
+
+	<!--**************** MAIN ****************-->
+	<div id="main" class="coworkingMain">
+		<!--**************** CONTENT ****************-->
+		<div id="content" class="clear">
+			<h1>Коворкинг</h1>
+				
+			<div class="section pricing">
+				<div class="sectionTitle">цены</div>
+				<div class="sectionContent">
+					<div class="pricingTypes">
+						<div class="pricingType">
+							<span class="titlePricingType">Фиксированное место</span>
+							<div class="listPricingType">
+								<div>1 день — 150 грн</div>
+								<div>5 дней — 725 грн</div>
+								<div>Месяц — 2899 грн</div>
+							</div>
+						</div>
+						<div class="pricingType">
+							<span class="titlePricingType">Плавающее место</span>
+							<div class="listPricingType">
+								<div>1 день — 150 грн</div>
+								<div>5 дней — 725 грн</div>
+								<div>Месяц — 2899 грн</div>
+							</div>
+						</div>
+					</div>
+					<div class="btnPricing"><a href="#">Забронировать</a></div>
+				</div>
+			</div>
+
+			<div class="section services">
+				<div class="sectionTitle">Услуги</div>
+				<div class="sectionContent">
+					<div class="itemsServices">
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем<br />круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/services/imgItemService.jpg" /></div>
+							<div class="title">Работаем круглосуточно</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+<!-- 			<div class="section">
+				<div class="sectionTitle">отзывы</div>
+				<div class="sectionContent">
+					12312312
+				</div>
+			</div> -->
+
+			<div class="section coworkingFriendly">
+				<div class="sectionTitle">Coworking friendly</div>
+				<div class="sectionContent">
+					<div class="descriptionCoworkingFriendly">Наши резиденты могут воспользоваться бесплатным днем для работы в коворкинг-сеть наших партнеров: во Львове, Харькове, Одессе и Минске.</div>
+					<div class="itemsCoworkingFriendly">
+						<div class="item">
+							<div class="city">Львов</div>
+							<div class="space">Betaplace</div>
+							<div class="img"><img src="img/friends/logoBetaplace.jpg" /></div>
+							<div class="itemContacts">
+								<a href="http://betaplace.com.ua" target="_blank">http://betaplace.com.ua</a>
+								<br />
+								<a href="mailto:info@betaplace.com.ua">info@betaplace.com.ua</a>
+								<br />
+								+380 (68) 686-88-25
+							</div>
+						</div>
+						<div class="item">
+							<div class="city">Львов</div>
+							<div class="space">Startup depot</div>
+							<div class="img"><img src="img/friends/logoStartupdepot.jpg" /></div>
+							<div class="itemContacts">
+								<a href="http://startupdepot.lviv.ua" target="_blank">http://startupdepot.lviv.ua</a>
+								<br />
+								<a href="mailto:office@startupdepot.lviv.ua">office@startupdepot.lviv.ua</a>
+								<br />
+								+38 (096) 96-93-641
+							</div>
+						</div>
+						<div class="item">
+							<div class="city">Харьков</div>
+							<div class="space">khackerspace</div>
+							<div class="img"><img src="img/friends/logoKhackerspace.png" /></div>
+							<div class="itemContacts">
+								<a href="http://www.khackerspace.org" target="_blank">http://www.khackerspace.org</a>
+								<br />
+								<a href="mailto:khackerspace@gmail.com">khackerspace@gmail.com</a>
+							</div>
+						</div>
+						<div class="item">
+							<div class="city">МИНСК</div>
+							<div class="space">IMAGURU</div>
+							<div class="img"><img src="img/friends/logoImaguru.png" /></div>
+							<div class="itemContacts">
+								<a href="http://imaguru.co" target="_blank">http://imaguru.co</a><br />
+								<a href="mailto:info@imaguru.co">info@imaguru.co</a><br />
+								+375 (29) 11-89-989
+							</div>
+						</div>
+						<div class="item">
+							<div class="city">Одесса</div>
+							<div class="space">Impact Hub</div>
+							<div class="img"><img src="img/friends/logoImpacthub.png" /></div>
+							<div class="itemContacts">
+								<a href="http://impacthub.odessa.ua" target="_blank">http://impacthub.odessa.ua</a>
+								<br />
+								<a href="hublab@impacthub.odessa.ua">hublab@impacthub.odessa.ua</a>
+								<br />
+								+38 (048) 70-33-555
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div><!-- end #content-->
+	</div><!-- end #main-->
+	<?php endwhile;?>
+
+<?php get_footer(); ?>
