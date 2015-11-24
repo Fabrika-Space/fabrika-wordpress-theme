f9976493ba880f4b2a5cc000cd195d50f0750578
NightNei <nei.css@gmail.com>
2015-11-24 17:59:59 +0200

    contactsPage

---
+++ fabrika.space/contacts.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Template Name: contacts
+*/
+?>
+<?php get_header(); ?>
+
+	<!--**************** MAIN ****************-->
+	<div id="main" class="pageStyleLikeHome contactsMain">
+		<!--**************** CONTENT ****************-->
+		<div id="content" class="clear">
+			<h1>Где нас искать</h1>
+
+			<div class="simpleTextBlock">
+				<p>Мы находимся в здании фабрики сортировки семян на улице Карла Маркса, 1 (рядом с Благовещенским собором), ближайшие станции метро – Центральный рынок, Исторический музей. Пешком от Южного железнодорожного вокзала – 7 минут.</p>
+			</div>
+
+		</div><!-- end #content-->
+	</div><!-- end #main-->
+
+<?php get_footer(); ?>
