b5b1158ee6ab510f69845f7bfab82694d958a59d
NightNei <nei.css@gmail.com>
2015-11-26 19:14:57 +0200

    add bar page

---
+++ fabrika.space/bar.php
@@ -0,0 +1,237 @@
+<?php
+/*
+Template Name: event-zone
+*/
+?>
+<?php get_header(); ?>
+
+	<!--**************** MAIN ****************-->
+	<div id="main" class="pageStyleLikeHome barMain">
+		<!--**************** CONTENT ****************-->
+		<div id="content" class="clear">
+			<h1>Бар</h1>
+
+			<div class="section aboutBar bgWhite clear">
+				<div class="sectionTitle borderBottomTitle">О нас</div>
+				<div class="sectionContent clear">
+					<div class="txtAboutBar">					
+						<p>Две фразы о чем речь. In lacinia, mi at dignissim volutpat, neque quam vestibulum arcu, sit amet placerat turpis libero id risus.</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum iaculis sodales orci, eget molestie lorem pharetra vel. Nam neque neque, rhoncus in tincidunt in, volutpat a lacus. Aliquam pulvinar fringilla posuere.</p>
+						<p>Maecenas dui ex, facilisis id lorem eget, gravida suscipit nunc. In at vehicula nisl. Suspendisse consequat, ligula mattis hendrerit vulputate.</p>
+					</div>
+					<div class="worktimeAboutBar">
+						<img src="img/services/imgServiceWorktime.png" />
+						<div class="txt">
+							<p>Работаем<br /> семь дней в неделю<br /> с девяти до девяти</p>
+							<p>Харьков,<br /> ул. Карла Маркса, 1</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="section reservation bgWhite">
+				<div class="sectionTitle borderBottomTitle">Бронь</div>
+				<div class="sectionContent">
+					<div class="data">
+						<div class="numberPeople">
+							<label class="select">
+								<select>
+									<option>на 1 человека</option>
+									<option>на 2 человека</option>
+									<option>на 3 человека</option>
+								</select>
+							</label>
+						</div>
+						<div class="date">
+							<label></label>
+							<input type="date">
+						</div>
+						<div class="time">
+							<label></label>
+							<input type="time" />
+						</div>
+						<div class="name">
+							<label>Имя</label>
+							<input type="text" placeholder="Иван Петров" />
+						</div>
+						<div class="phone">
+							<label>Ваш телефон</label>
+							<input type="text" placeholder="+38 (      )" />
+						</div>
+					</div>
+
+					<div class="ourTel">
+						<span class="title">Наш телефон: </span>
+						<span class="phone">+38 (123) 456-78-90</span>
+						<span class="desc">(на случай если вы захотите что-то уточнить)</span>
+					</div>
+					<div class="textareaComment">
+						<textarea placeholder="Пожелания (если есть)"></textarea>
+					</div>
+
+					<div class="note">Заявка будет обработана в течении 15 минут.<br /> Если возникнут вопросы, мы вам перезвоним.</div>
+					<div class="submit"><input type="submit" value="Забронировать столик" /></div>
+				</div>
+			</div>
+
+			<div class="section menuBar bgWhite">
+				<div class="sectionTitle borderBottomTitle">Меню</div>
+				<div class="sectionContent clear">
+					
+					<div class="contentMenuBar">
+						<div class="categoryMenuBar">
+							<div class="titleCategoryMenuBar act">Short drinks</div>
+							<div class="bodyCategoryMenuBar">
+								<div class="listTxtItems">
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+									<div class="txtItem">
+										<span class="price">50 грн</span>
+										<span class="title">WHISKEY SOUR 90 ml</span>
+										<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="categoryMenuBar">
+							<div class="titleCategoryMenuBar act">Long Drinks</div>
+							<div class="bodyCategoryMenuBar">
+								<div class="listBeautifulItems">
+									<div class="itemWithImage">
+										<div class="data">
+											<span class="title"><div><span>Bacardi Mojito</span></div></span>
+											<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+											<span class="price">50 грн</span>
+										</div>
+										<div class="img"><img src="img/imgMenuBarItem1.jpg" /></div>
+									</div>
+									<div class="itemWithImage">
+										<div class="data">
+											<span class="title"><div><span>William Lawson's Cola</span></div></span>
+											<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+											<span class="price">50 грн</span>
+										</div>
+										<div class="img"><img src="img/imgMenuBarItem2.jpg" /></div>
+									</div>
+									<div class="itemWithImage">
+										<div class="data">
+											<span class="title"><div><span>WHISKEY</span></div></span>
+											<span class="desc">Bourbon whiskey, lemon fresh, sugar syrup, protein</span>
+											<span class="price">50 грн</span>
+										</div>
+										<div class="img"><img src="img/imgMenuBarItem3.jpg" /></div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="rightSidebarMenuBar">
+						<div class="moduleRightSidebarMenuBar">
+							<span class="h2">Новые напитки</span>
+							<div class="item">
+								<div class="txt">
+									<span class="title">В связи с холодами у нас появился глинтвейн</span>
+									<span class="price"><b>35</b> грн</span>
+								</div>
+								<div class="img"><img src="img/imgModuleRightSidebarMenuBar.jpg"></div>
+								<span class="desc">Состав: красное/белое вино, коньяк, цедра апельсина, яблоки, корица молотая, корица-палочка, апельсиновый сок, мёд, звёздочка бадьяна и гвоздика</span>
+							</div>
+							<div class="item">
+								<div class="txt">
+									<span class="title">Медный всадник</span>
+									<span class="price"><b>35</b> грн</span>
+								</div>
+								<div class="img"><img src="img/imgModuleRightSidebarMenuBar.jpg"></div>
+								<span class="desc">Состав: кальвадос, апельсиновый джем, анисовая настойка перно, сок лимона, яблочный сок и яблочный сироп</span>
+							</div>
+						</div>
+						<div class="moduleRightSidebarMenuBar">
+							<span class="h2">Акции</span>
+							<div class="item">
+								<div class="txt">
+									<span class="title">Сэт «Бронепоезд» 5 шт</span>
+									<span class="price"><b>35</b> грн</span>
+								</div>
+								<div class="img"><img src="img/imgModuleRightSidebarMenuBar.jpg"></div>
+								<span class="desc">Шот сет из 5 шотов за 100 грн.</span>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+			<div class="section staffBar bgWhite">
+				<div class="sectionTitle borderBottomTitle">Персонал</div>
+				<div class="sectionContent">
+					<div class="listStaffBar">
+						<div class="item">
+							<div class="img"><img src="img/imgBarmen1.jpg" height="300" /></div>
+							<span class="workPosition">Барриста</span>
+							<span class="name">Иван Петров</span>
+							<span class="desc">многократный призер-чемпионер</span>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/imgBarmen2.jpg" height="300" /></div>
+							<span class="workPosition">Бармен</span>
+							<span class="name">Иван Петров</span>
+							<span class="desc">на все руки мастер</span>
+						</div>
+						<div class="item">
+							<div class="img"><img src="img/imgBarmen3.jpg" height="300" /></div>
+							<span class="workPosition">Администратор</span>
+							<span class="name">Иван Петров</span>
+							<span class="desc">держит руку на пульсе</span>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div><!-- end #content-->
+	</div><!-- end #main-->
+
+<?php get_footer(); ?>
