6f6891fdece316826a4ddf6c89213a7ca3506d1c
NightNei <nei.css@gmail.com>
2015-11-26 14:25:38 +0200

    add markup eventzone

---
+++ fabrika.space/eventzone.php
@@ -0,0 +1,244 @@
+<?php
+/*
+Template Name: coworking
+*/
+?>
+<?php get_header(); ?>
+
+<!--**************** MAIN ****************-->
+	<div id="main" class="pageStyleLikeHome eventzoneMain">
+		<!--**************** CONTENT ****************-->
+		<div id="content" class="clear">
+			<h1>Ивент-зоны</h1>
+
+			<div class="section eventRooms bgWhite">
+				<div class="sectionTitle borderBottomTitle">три пространства в фабрике</div>
+				<div class="sectionContent">
+					<div class="descEventRooms">У вас есть возможность воспользоваться тремя основными ивент-зонами</div>
+					<div class="listEventRooms">
+						<div class="item">
+							<div class="title">Красная зона<br /> на 450 человек<br /> на третьем этаже</div>
+							<div class="img"><img src="/wp-content/themes/fabrika.space/img/imgEventroomRed.png" /></div>
+							<div class="moreDetails"><a href="#">Подробнее</a></div>
+						</div>
+						<div class="item">
+							<div class="title">Синяя зона<br /> на 150 человек<br /> на втором этаже</div>
+							<div class="img"><img src="/wp-content/themes/fabrika.space/img/imgEventroomBlue.png" /></div>
+							<div class="moreDetails"><a href="#">Подробнее</a></div>
+						</div>
+						<div class="item">
+							<div class="title">Зеленая зона<br /> на 50 человек<br /> на втором этаже</div>
+							<div class="img"><img src="/wp-content/themes/fabrika.space/img/imgEventroomGreen.png" /></div>
+							<div class="moreDetails"><a href="#">Подробнее</a></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="section services">
+				<div class="sectionTitle borderBottomTitle">Услуги</div>
+				<div class="sectionContent">
+					<div class="itemsServices">
+						<div class="item">
+							<div class="img"><img src="/wp-content/themes/fabrika.space/img/services/imgServiceInternet.png"></div>
+							<div class="title">бесплатный<br> скоростной<br> интернет</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="/wp-content/themes/fabrika.space/img/services/imgServiceProjector.png"></div>
+							<div class="title">мультимедиапроектор</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="/wp-content/themes/fabrika.space/img/services/imgServiceMicrophone.png"></div>
+							<div class="title">микрофоны<br /> и аудиосистема<br /> (кроме зала<br /> на 50 человек)</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="/wp-content/themes/fabrika.space/img/services/imgServiceCoffeeAndTea.png"></div>
+							<div class="title">организация<br /> кофе-бреков</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="/wp-content/themes/fabrika.space/img/services/imgServiceMarkerBoard.png"></div>
+							<div class="title">маркерная доска</div>
+						</div>
+						<div class="item">
+							<div class="img"><img src="/wp-content/themes/fabrika.space/img/services/imgServiceVideorecording.png"></div>
+							<div class="title">возможность<br /> видеозаписи</div>
+						</div>
+					</div>
+
+					<div class="roomsDescription">
+						<div class="item">
+							<div class="img" style="background-image: url('/wp-content/themes/fabrika.space/img/imgRoomsDescription.jpg'), url('/wp-content/themes/fabrika.space/img/lineRedRoom.png');"></div>
+							<div class="txt">
+								<p><span class="name">Красная зона</span> предназначена для проведения семинаров, симпозиумов, конференций, обучающих мероприятий и может вместить до 450 человек при разных вариантах рассадки. Конференц-зал позволяет проводить мероприятия с использованием новейшего мультимедийного оборудования, т.к. он оснащен панелью из видеокубов Planar – широкоэкранных проекционных модулей, которые могут воспроизводить изображение высокого разрешения.</p>
+								<p>Дисплеи Planar разработаны с таким расчетом, что бы получить яркое равномерное изображение при любых инсталляциях. На протяжении всего мероприятия осуществляется техническая поддержка осуществляется высококвалифицированными специалистами Фабрики.</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="img" style="background-image: url('/wp-content/themes/fabrika.space/img/imgRoomsDescription.jpg'), url('/wp-content/themes/fabrika.space/img/lineBlueRoom.png');"></div>
+							<div class="txt">
+								<p><span class="name">Синяя зона</span> рассчитана на 150 человек и как нельзя лучше подходит для проведения встреч с прессой, переговоров, совещаний, небольших конференций и подписания документов. Пресс-центр как и большой Конференц-холл, оснащен техническим оборудованием, необходимым для проведения переговоров и презентаций. Вы можете провести презентацию на территории Пресс-центра, т.к. он располагает  большой видеопанелью, размером 130 дюймов и микрофонами, так же к услугам гостей Фабрики – возможность подключения к Internet. Помимо большой комнаты для переговоров, Пресс-центр включает в себя еще малую переговорную комнату, а также VIP-переговорную.</p>
+
+								<p>VIP-переговорная представляет собой небольшое уютное помещение, оформленное в лаконичных черно-белых тонах, в котором будет удобно даже самым требовательным гостям. VIP-переговорная, вместимостью до 8 человек подходит как для отдыха, так и для небольшой деловой встречи, в ней изысканная кожаная мебель, а также отдельный санузел.</p>
+							</div>
+						</div>
+						<div class="item">
+							<div class="img" style="background-image: url('/wp-content/themes/fabrika.space/img/imgRoomsDescription.jpg'), url('/wp-content/themes/fabrika.space/img/lineGreenRoom.png');"></div>
+							<div class="txt">
+								<p><span class="name">Зеленая зона</span> рассчитана на 50 человек и как нельзя лучше подходит для проведения встреч с прессой, переговоров, совещаний, небольших конференций и подписания документов. Пресс-центр как и большой Конференц-холл, оснащен техническим оборудованием, необходимым для проведения переговоров и презентаций.</p>
+								<p>Вы можете провести презентацию на территории Пресс-центра, т.к. он располагает  большой видеопанелью, размером 130 дюймов и микрофонами, так же к услугам гостей ИТ парка – возможность подключения к Internet. Помимо большой комнаты для переговоров, Пресс-центр включает в себя еще малую переговорную комнату, а также VIP-переговорную.</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="sectionTitle">цены</div>
+					<div class="sectionContent">
+						<div class="priceCategory">
+							<div class="body">
+								<div class="typePriceCategories">
+									<div class="typePriceCategory">
+										<span class="title">Красная зона</span>
+										<span class="countPeople">450 человек</span>
+										<div class="table">
+											<table>
+												<tr>
+													<td>Целый день</td>
+													<td>—</td>
+													<td>2500 грн</td>
+												</tr>
+												<tr>
+													<td>8 часов</td>
+													<td>—</td>
+													<td>416 грн</td>
+												</tr>
+												<tr>
+													<td>4 часа</td>
+													<td>—</td>
+													<td>550 грн</td>
+												</tr>
+											</table>
+										</div>
+									</div>
+									<div class="typePriceCategory">
+										<span class="title">Зеленая зона</span>
+										<span class="countPeople">150 человек</span>
+										<div class="table">
+											<table>
+												<tr>
+													<td>Целый день</td>
+													<td>—</td>
+													<td>2500 грн</td>
+												</tr>
+												<tr>
+													<td>8 часов</td>
+													<td>—</td>
+													<td>416 грн</td>
+												</tr>
+												<tr>
+													<td>4 часа</td>
+													<td>—</td>
+													<td>550 грн</td>
+												</tr>
+											</table>
+										</div>
+									</div>
+									<div class="typePriceCategory">
+										<span class="title">Синяя зона</span>
+										<span class="countPeople">50 человек</span>
+										<div class="table">
+											<table>
+												<tr>
+													<td>Целый день</td>
+													<td>—</td>
+													<td>2500 грн</td>
+												</tr>
+												<tr>
+													<td>8 часов</td>
+													<td>—</td>
+													<td>416 грн</td>
+												</tr>
+												<tr>
+													<td>4 часа</td>
+													<td>—</td>
+													<td>550 грн</td>
+												</tr>
+											</table>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="btnPricing"><a href="#">Забронировать</a></div>
+					<div class="txtHelpPricing">Что-либо уточнить вы можете по телефону — 8 050 123 45 67</div>
+				</div>
+			</div>
+			
+			<div class="section plansAndPhotosEventRooms bgWhite">
+				<div class="sectionTitle">Планы и фото ивент-зон</div>
+				<div class="sectionContent">
+					<div class="item clear">
+						<div class="title bgTransparentRed">Красная зона</div>
+						<div class="img"><img src="/wp-content/themes/fabrika.space/img/imgRedBlueGreenRoom.jpg" /></div>
+						<div class="map"><img src="/wp-content/themes/fabrika.space/img/smallMapRedRoom.jpg" /></div>
+						<div class="lastEvent">
+							<div class="titleLastEvent">
+								<div>Последнее событие в этой зоне</div>
+								<div><b>Kharkiv JS Conf</b></div>
+							</div>
+							<div class="imgLastEvent"><img src="/wp-content/themes/fabrika.space/img/imgLastEvent.jpg" /></div>
+							<div class="txtLastEvent">Ut nec felis ut quam eleifend fermentum a eget velit. Fusce sed rutrum felis. Sed et rhoncus odio.</div>
+						</div>
+					</div>
+					<div class="item clear">
+						<div class="title bgTransparentBlue">синяя зона</div>
+						<div class="img"><img src="/wp-content/themes/fabrika.space/img/imgRedBlueGreenRoom.jpg" /></div>
+						<div class="map"><img src="/wp-content/themes/fabrika.space/img/smallMapBlueRoom.jpg" /></div>
+						<div class="lastEvent">
+							<div class="titleLastEvent">
+								<div>Последнее событие в этой зоне</div>
+								<div><b>Kharkiv JS Conf</b></div>
+							</div>
+							<div class="imgLastEvent"><img src="/wp-content/themes/fabrika.space/img/imgLastEvent.jpg" /></div>
+							<div class="txtLastEvent">Ut nec felis ut quam eleifend fermentum a eget velit. Fusce sed rutrum felis. Sed et rhoncus odio.</div>
+						</div>
+					</div>
+					<div class="item clear">
+						<div class="title bgTransparentGreen">Зеленая зона</div>
+						<div class="img"><img src="/wp-content/themes/fabrika.space/img/imgRedBlueGreenRoom.jpg" /></div>
+						<div class="map"><img src="/wp-content/themes/fabrika.space/img/smallMapGreenRoom.jpg" /></div>
+						<div class="lastEvent">
+							<div class="titleLastEvent">
+								<div>Последнее событие в этой зоне</div>
+								<div><b>Kharkiv JS Conf</b></div>
+							</div>
+							<div class="imgLastEvent"><img src="/wp-content/themes/fabrika.space/img/imgLastEvent.jpg" /></div>
+							<div class="txtLastEvent">Ut nec felis ut quam eleifend fermentum a eget velit. Fusce sed rutrum felis. Sed et rhoncus odio.</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="section ourClients bgWhite">
+				<div class="sectionTitle borderBottomTitle">Наши клиенты</div>
+				<div class="sectionContent">
+					<div class="listOurClients">
+						<div class="item">
+							<img src="/wp-content/themes/fabrika.space/img/ourClients/imgClientEpam.png" />
+						</div>
+						<div class="item">
+							<img src="/wp-content/themes/fabrika.space/img/ourClients/imgClientGloballogic.png" />
+						</div>
+						<div class="item">
+							<img src="/wp-content/themes/fabrika.space/img/ourClients/imgClentToptechphoto.png" />
+						</div>
+						<div class="item">
+							<img src="/wp-content/themes/fabrika.space/img/ourClients/imgClientCiklum.png" />
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div><!-- end #content-->
+	</div><!-- end #main-->
+
+<?php get_footer(); ?>
