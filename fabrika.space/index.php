6c7aff1b0abd9287fde10ab3ef0e0b64f090e38a
NightNei <nei.css@gmail.com>
2016-02-08 17:27:04 +0200

    Revert sort

--- fabrika.space/index.php
+++ fabrika.space/index.php
@@ -162,57 +162,44 @@ get_header(); ?>
 					<div class="rightArrow"></div>
 					
 					<div class="listItems">
-
 						<div class="innerListItems">
 							<?php
-								$querystr = "
-											SELECT wposts.*
-											FROM $wpdb->posts wposts, $wpdb->postmeta wpostmeta
-											WHERE wposts.ID = wpostmeta.post_id
-											AND wpostmeta.meta_key = 'EventDate'
-											AND wposts.post_status = 'publish'
-											AND wposts.post_type = 'post'
-											ORDER BY STR_TO_DATE(wpostmeta.meta_value, '%YYYY-%mm-%dd') ASC
-											";
+								$args = array( 'category_name' => 'Event', 'posts_per_page' => -1, 'orderby' => 'post_date', 'order' => 'ASC' );
+								$postslist = get_posts( $args );
+								$no_active = true;
+								$now_date = date_parse(date("Y-m-d"));
 
-								$pageposts = $wpdb->get_results($querystr, OBJECT);
+								foreach ( $postslist as $post ) :
+								  setup_postdata( $post ); 
+									$eventDate = date_parse(get_post_meta($post->ID, 'EventDate', true));
 
-								if ($pageposts){
-									$no_active = true;
-									$now_date = date_parse(date("Y-m-d"));
+									if($no_active && $eventDate >= $now_date){
+										$no_active = false;
+									?>
+										<a href="<?php the_permalink(); ?>" class="item act">
 
-									foreach ( $pageposts as $post ) :
-									  setup_postdata( $post ); 
-										$eventDate = date_parse(get_post_meta($post->ID, 'EventDate', true));
+									<?php }else{ ?>
 
-										if($no_active && $eventDate >= $now_date){
-											$no_active = false;
-										?>
-											<a href="<?php the_permalink(); ?>" class="item act">
+										<a href="<?php the_permalink(); ?>" class="item">
+									<?php } ?>
 
-										<?php }else{ ?>
-
-											<a href="<?php the_permalink(); ?>" class="item">
-										<?php } ?>
-
-											<div class="itemImg">
-												<img src="<?php echo get_post_meta($post->ID, 'Photo', true); ?>" />
+										<div class="itemImg">
+											<img src="<?php echo get_post_meta($post->ID, 'Photo', true); ?>" />
+										</div>
+										<div class="itemContent">
+											<div class="title"><div><span><?php the_title(); ?></span></div></div>
+											<div class="dateTime">
+												<span class="time">
+													<span>с</span><?php echo get_post_meta($post->ID, 'EventStartTime', true); ?><br /><span>по</span><?php echo get_post_meta($post->ID, 'EventEndTime', true); ?>
+												</span>
+												<span class="date"><?php echo $eventDate['day']; ?></span>
+												<span class="month"><?php echo date('F', mktime(0, 0, 0, $eventDate['month'], 10)); ?></span>
 											</div>
-											<div class="itemContent">
-												<div class="title"><div><span><?php the_title(); ?></span></div></div>
-												<div class="dateTime">
-													<span class="time">
-														<span>с</span><?php echo get_post_meta($post->ID, 'EventStartTime', true); ?><br /><span>по</span><?php echo get_post_meta($post->ID, 'EventEndTime', true); ?>
-													</span>
-													<span class="date"><?php echo $eventDate['day']; ?></span>
-													<span class="month"><?php echo date('F', mktime(0, 0, 0, $eventDate['month'], 10)); ?></span>
-												</div>
-											</div>
-										</a>
-									<?php
+										</div>
+									</a>
+								<?php
 
-									endforeach;
-								}
+								endforeach; 
 								wp_reset_postdata();
 							?>
 
