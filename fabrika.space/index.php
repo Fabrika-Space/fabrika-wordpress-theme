337166307818a055e1402b1a1ad64b1e1d271b6f
Johndegree <john@toptechphoto.com>
2016-02-10 19:51:58 +0200

    Change carousel images.

--- fabrika.space/index.php
+++ fabrika.space/index.php
@@ -11,10 +11,10 @@ get_header(); ?>
 			<!--**************** SLIDER ****************-->
 			<div class="slider">
 				<div class="innerSlider">
-					<div class="sliderItem" style="background-image: url('/wp-content/themes/fabrika.space/img/sliderHomePage/imgSliderBar.jpg');" data-title="1 Этаж - Бар"></div>
-					<div class="sliderItem" style="background-image: url('/wp-content/themes/fabrika.space/img/sliderHomePage/imgSliderOffices.jpg');" data-title="2 Этаж - Офисы"></div>
+					<div class="sliderItem" style="background-image: url('/wp-content/themes/fabrika.space/img/sliderHomePage/cosmopolitan.jpg');" data-title="1 Этаж - Бар"></div>
+					<div class="sliderItem" style="background-image: url('/wp-content/themes/fabrika.space/img/sliderHomePage/eventzone2.jpg');" data-title="2 Этаж - Ивент-зона"></div>
 					<div class="sliderItem" style="background-image: url('/wp-content/themes/fabrika.space/img/sliderHomePage/imgSliderConferenceService.jpg');" data-title="3 Этаж - Ивент-зона"></div>
-					<div class="sliderItem" style="background-image: url('/wp-content/themes/fabrika.space/img/sliderHomePage/imgSliderCoworking.jpg');" data-title="4 Этаж - Коворкинг"></div>
+					<div class="sliderItem" style="background-image: url('/wp-content/themes/fabrika.space/img/sliderHomePage/coworking.jpg');" data-title="4 Этаж - Коворкинг"></div>
 				</div>
 
 				<div class="arrows">
