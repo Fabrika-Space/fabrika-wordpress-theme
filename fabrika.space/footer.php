697a9f18873291140c98b44e1da196fe19c3617b
NightNei <nei.css@gmail.com>
2016-04-09 16:07:39 +0300

    add subscribe button to footer

--- fabrika.space/footer.php
+++ fabrika.space/footer.php
@@ -27,9 +27,34 @@
 				<div class="part">
 					<div class="item"><a href="tel:+380681001144" class="icon_tel">+38 (068) 100 11 44</a></div>
 					<div class="item"><a href="tel:+380995023246" class="icon_tel">+38 (099) 502 32 46</a></div>
+					<div class="item"><a href="mailto:info@fabrika.space" class="icon_mail">info@fabrika.space</a></div>
 				</div>
 				<div class="part">
-					<div class="item"><a href="mailto:info@fabrika.space" class="icon_mail">info@fabrika.space</a></div>
+					<div class="item">Будь в курсе последних событий</div>
+					<div class="item">
+						<!-- <input type="text" placeholder="email" />
+						<input type="submit" placeholder="Subscribe" /> -->
+
+						<!-- Begin MailChimp Signup Form -->
+						<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+						<style type="text/css">
+						    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; width:100%;}
+						    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+						       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+						</style>
+						<div id="mc_embed_signup">
+						<form action="//space.us12.list-manage.com/subscribe/post?u=b8c7a127bdc373e0aa6813c5d&amp;id=fe24f30006" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						    <div id="mc_embed_signup_scroll">
+						    <label for="mce-EMAIL">Еженедельная рассылка лучших событий Fabrika.space</label>
+						    <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+						    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b8c7a127bdc373e0aa6813c5d_fe24f30006" tabindex="-1" value=""></div>
+						    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						    </div>
+						</form>
+						</div>
+						<!--End mc_embed_signup-->
+					</div>
 				</div>
 				<!--div class="part">
 					<div class="item"><a href="tel:80501234567" class="icon_tel">8 (050) 123 45 67</a></div>
