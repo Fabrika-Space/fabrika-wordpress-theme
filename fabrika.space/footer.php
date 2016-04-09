16cff58f92b0f054e19a16204d1245880c17089e
NightNei <nei.css@gmail.com>
2016-04-09 16:58:15 +0300

    new subscribe code

--- fabrika.space/footer.php
+++ fabrika.space/footer.php
@@ -32,7 +32,7 @@
 				<div class="part">
 					<label class="item" for="mce-EMAIL">Будь в курсе последних событий</label>
 					<div class="item">
-						<link href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css" rel="stylesheet" type="text/css">
+						<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
 						<style type="text/css">
 							#mc_embed_signup form {
 								text-align: left;
@@ -66,14 +66,23 @@
 							}
 						</style>
 						<div id="mc_embed_signup">
-							<form action="//space.us12.list-manage.com/subscribe/post?u=b8c7a127bdc373e0aa6813c5d&amp;id=fe24f30006" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+							<form action="//space.us12.list-manage.com/subscribe/post?u=b8c7a127bdc373e0aa6813c5d&amp;id=b4163bb809" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 								<div id="mc_embed_signup_scroll">
-									<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email" required>
-									<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b8c7a127bdc373e0aa6813c5d_fe24f30006" tabindex="-1" value=""></div>
-									<div class="clear"><input type="submit" value="<?php echo($lang=='ru'?'Подписаться':'Subscribe') ?>" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+									<div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+									<div class="mc-field-group">
+										<label for="mce-EMAIL">Email Address  <span class="asterisk">*</span></label>
+										<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+									</div>
+									<div id="mce-responses" class="clear">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none"></div>
+									</div>
+									<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_b8c7a127bdc373e0aa6813c5d_b4163bb809" tabindex="-1" value=""></div>
+									<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 								</div>
 							</form>
 						</div>
+						<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
 					</div>
 				</div>
 				<!--div class="part">
