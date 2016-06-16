69e2dfaa3261945a178b600a1dc5dbfa726dfa75
Johndegree <john@toptechphoto.com>
2016-06-16 14:27:36 +0300

    Remove outdated items

--- fabrika.space/bar.php
+++ fabrika.space/bar.php
@@ -95,15 +95,6 @@ Template Name: bar
 										<span class="composition">(140 ml) Gin, lime cordial, cilantro, cucumber, egg white, mascarpone</span>
 										<span class="desc">description</span>
 									</div>
-									<!--div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>AMIGO</span></div></span>
-											<span class="price">100 uah</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barAmigo.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(135 ml) tequila, agave sec, cranberry, gomme syrup, lime, egg white, cardamom</span>
-										<span class="desc">description</span>
-									</div-->
 									<div class="itemWithImage">
 										<div class="data">
 											<span class="title"><div><span>PEAR SOUR</span></div></span>
@@ -113,52 +104,6 @@ Template Name: bar
 										<span class="composition">(150 ml) bourbon, pear cordial, lime, egg white</span>
 										<span class="desc">description</span>
 									</div>
-									<!--div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>WILD SOUR</span></div></span>
-											<span class="price">85 uah</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barWildSour.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(120 ml) grand marnier, lime cordial, sugar, egg white, rosemary</span>
-										<span class="desc">description</span>
-									</div-->
-									<!--div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>PASSION APHRODITE</span></div></span>
-											<span class="price">120 uah</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barPassionAphrodite.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(200 ml) pineapple, baileys, grape brandy, mascarpone</span>
-										<span class="desc">description</span>
-									</div-->
-									<!--div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>PINK FUME</span></div></span>
-											<span class="price">90 uah</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barPinkFume.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(100 ml) yogurt liquor, sloe gin, amaretto, mascarpone</span>
-										<span class="desc">description</span>
-									</div>
-									<div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>HLEB</span></div></span>
-											<span class="price">90 uah</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barHleb.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(110 ml) rye bread bourbon, lime, gomme syrup, prune, bread</span>
-										<span class="desc">description</span>
-									</div>
-									<div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>RYE OLD FASHIONED</span></div></span>
-											<span class="price">135 uah</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barRyeOldFashioned.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(80 ml) rye bourbon, whiskey barrel bitters, sugar</span>
-										<span class="desc">description</span>
-									</div-->
-
 								</div>
 							</div>
 						</div>
@@ -240,11 +185,6 @@ Template Name: bar
 									</div>
 								</div>
 								<div class="listTxtItems">
-									<!--div class="txtItem">
-										<span class="price">80 uah</span>
-										<span class="title">CILANTRO SOUR 110 ml </span>
-										<span class="desc">Bourbon, cilantro cordial, egg white</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">84 uah</span>
 										<span class="title">CLOVER CLUB 135 ml</span>
@@ -260,11 +200,6 @@ Template Name: bar
 										<span class="title">WHISKEY SOUR 140 ml</span>
 										<span class="desc">Bourbon, lime cordial, egg white</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">100 uah</span>
-										<span class="title">IFIZZ 140 ml</span>
-										<span class="desc">Quince gin, lime cordial, egg white, soda water</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">95 uah</span>
 										<span class="title">WHITE RUSSIAN 130 ml</span>
@@ -280,11 +215,6 @@ Template Name: bar
 										<span class="title">BOULEVARDIER 90 ml</span>
 										<span class="desc">Bourbon, campari, vermouth rosso</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">100 uah</span>
-										<span class="title">BOULEVARDIER PRUNES (basic/spicy) 90 ml</span>
-										<span class="desc">Bourbon, campari, vermouth rosso, tobacco bitters, prunes</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">90 uah</span>
 										<span class="title">COSMOPOLITAN 110 ml</span>
@@ -305,11 +235,6 @@ Template Name: bar
 										<span class="title">MANHATTAN 100 ml</span>
 										<span class="desc">Bourbon, vermouth rosso, angostura bitters</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">140 uah</span>
-										<span class="title">PERFECT MANHATTAN 90 ml</span>
-										<span class="desc">Rye bourbon, vermouth rosso, whisky barrel bitters</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">90 uah</span>
 										<span class="title">BLOODY MARY 220 ml</span>
@@ -356,11 +281,6 @@ Template Name: bar
 										<span class="title">MULLED WINE 210 ml</span>
 										<span class="desc">Red dry wine, sea buckthorns, raspberry, orange, lemon, cinnamon, clove, honey</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">100 uah</span>
-										<span class="title">CINNAMON CREAM 160 ml</span>
-										<span class="desc">cinnamon whiskey, drambuie, caramel, apple, cream, nutmeg</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">75 uah</span>
 										<span class="title">IRISH COFFEE 200 ml</span>
@@ -389,16 +309,6 @@ Template Name: bar
 										<span class="title">CITRUS LEMONADE 250 ml</span>
 										<span class="desc">Lemongrass, fresh-squeezed lime juice, fresh-squeezed orange juice</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">50 uah</span>
-										<span class="title">RASPBERRY LEMONADE 200 ml</span>
-										<span class="desc">Raspberry, ginger, peach juice, honey syrup, soda water</span>
-									</div>
-									<div class="txtItem">
-										<span class="price">50 uah</span>
-										<span class="title">ORANGE LEMONADE 200 ml</span>
-										<span class="desc">Orange, ginger, passion fruit syrup, soda water</span>
-									</div-->
 								</div>
 							</div>
 						</div>
@@ -500,15 +410,6 @@ Template Name: bar
 										<span class="composition">(140 ml) gin, lime cordial, cilantro, cucumber, egg white, mascarpone</span>
 										<span class="desc">description</span>
 									</div>
-									<!--div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>AMIGO</span></div></span>
-											<span class="price">100 грн</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barAmigo.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(135 ml) tequila, agave sec, cranberry, gomme syrup, lime, egg white, cardamom</span>
-										<span class="desc">description</span>
-									</div-->
 									<div class="itemWithImage">
 										<div class="data">
 											<span class="title"><div><span>PEAR SOUR</span></div></span>
@@ -518,51 +419,6 @@ Template Name: bar
 										<span class="composition">(150 ml) bourbon, pear cordial, lime, egg white</span>
 										<span class="desc">description</span>
 									</div>
-									<!--div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>WILD SOUR</span></div></span>
-											<span class="price">85 грн</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barWildSour.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(120 ml) grand marnier, lime cordial, sugar, egg white, rosemary</span>
-										<span class="desc">description</span>
-									</div-->
-									<!--div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>PASSION APHRODITE</span></div></span>
-											<span class="price">120 грн</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barPassionAphrodite.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(200 ml) pineapple, baileys, grape brandy, mascarpone</span>
-										<span class="desc">description</span>
-									</div-->
-									<!--div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>PINK FUME</span></div></span>
-											<span class="price">90 грн</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barPinkFume.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(100 ml) yogurt liquor, sloe gin, amaretto, mascarpone</span>
-										<span class="desc">description</span>
-									</div>
-									<div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>HLEB</span></div></span>
-											<span class="price">90 грн</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barHleb.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(110 ml) rye bread bourbon, lime, gomme syrup, prune, bread</span>
-										<span class="desc">description</span>
-									</div>
-									<div class="itemWithImage">
-										<div class="data">
-											<span class="title"><div><span>RYE OLD FASHIONED</span></div></span>
-											<span class="price">135 грн</span>
-										</div>
-										<div class="img"><img src="/wp-content/themes/fabrika.space/img/barRyeOldFashioned.jpg" data-popupGalery="1" /></div>
-										<span class="composition">(80 ml) rye bourbon, whiskey barrel bitters, sugar</span>
-										<span class="desc">description</span>
-									</div-->
 
 								</div>
 							</div>
@@ -645,11 +501,6 @@ Template Name: bar
 									</div>
 								</div>
 								<div class="listTxtItems">
-									<!--div class="txtItem">
-										<span class="price">80 грн</span>
-										<span class="title">CILANTRO SOUR 110 ml </span>
-										<span class="desc">Bourbon, cilantro cordial, egg white</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">84 грн</span>
 										<span class="title">CLOVER CLUB 135 ml</span>
@@ -665,11 +516,6 @@ Template Name: bar
 										<span class="title">WHISKEY SOUR 140 ml</span>
 										<span class="desc">Bourbon, lime cordial, egg white</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">100 грн</span>
-										<span class="title">IFIZZ 140 ml</span>
-										<span class="desc">Quince gin, lime cordial, egg white, soda water</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">95 грн</span>
 										<span class="title">WHITE RUSSIAN 130 ml</span>
@@ -685,11 +531,6 @@ Template Name: bar
 										<span class="title">BOULEVARDIER 90 ml</span>
 										<span class="desc">Bourbon, campari, vermouth rosso</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">100 грн</span>
-										<span class="title">BOULEVARDIER PRUNES (basic/spicy) 90 ml</span>
-										<span class="desc">Bourbon, campari, vermouth rosso, tobacco bitters, prunes</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">90 грн</span>
 										<span class="title">COSMOPOLITAN 110 ml</span>
@@ -710,11 +551,6 @@ Template Name: bar
 										<span class="title">MANHATTAN 100 ml</span>
 										<span class="desc">Bourbon, vermouth rosso, angostura bitters</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">140 грн</span>
-										<span class="title">PERFECT MANHATTAN 90 ml</span>
-										<span class="desc">Rye bourbon, vermouth rosso, whisky barrel bitters</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">90 грн</span>
 										<span class="title">BLOODY MARY 220 ml</span>
@@ -761,11 +597,6 @@ Template Name: bar
 										<span class="title">MULLED WINE 210 ml</span>
 										<span class="desc">Red dry wine, sea buckthorns, raspberry, orange, lemon, cinnamon, clove, honey</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">100 грн</span>
-										<span class="title">CINNAMON CREAM 160 ml</span>
-										<span class="desc">cinnamon whiskey, drambuie, caramel, apple, cream, nutmeg</span>
-									</div-->
 									<div class="txtItem">
 										<span class="price">75 грн</span>
 										<span class="title">IRISH COFFEE 200 ml</span>
@@ -794,16 +625,6 @@ Template Name: bar
 										<span class="title">CITRUS LEMONADE 250 ml</span>
 										<span class="desc">Lemongrass, fresh-squeezed lime juice, fresh-squeezed orange juice</span>
 									</div>
-									<!--div class="txtItem">
-										<span class="price">50 грн</span>
-										<span class="title">RASPBERRY LEMONADE 200 ml</span>
-										<span class="desc">Raspberry, ginger, peach juice, honey syrup, soda water</span>
-									</div>
-									<div class="txtItem">
-										<span class="price">50 грн</span>
-										<span class="title">ORANGE LEMONADE 200 ml</span>
-										<span class="desc">Orange, ginger, passion fruit syrup, soda water</span>
-									</div-->
 								</div>
 							</div>
 						</div>
@@ -819,7 +640,6 @@ Template Name: bar
 									<span class="price"><b>40</b> грн</span>
 								</div>
 								<div class="img"><img src="/wp-content/themes/fabrika.space/img/barCacao.jpg"></div>
-								<!--span class="desc">Состав: красное/белое вино, коньяк, цедра апельсина, яблоки, корица молотая, корица-палочка, апельсиновый сок, мёд, звёздочка бадьяна и гвоздика</span-->
 							</div>
 						</div>
 					</div>
